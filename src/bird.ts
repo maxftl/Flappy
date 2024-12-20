19307a52ece06282e25e0fd4160c1aa7b5be2071
maximilian <mfichtl@gmail.com>
2024-12-20 14:40:11 +0100

    basic bird rotation

--- src/bird.ts
+++ src/bird.ts
@@ -13,11 +13,15 @@ export class Bird implements Drawable {
     currentFrame: HTMLImageElement;
     x: number;
     y: number;
+    width: number;
+    height: number;
     velocity: number;
 
     constructor(frames: BirdFrames) {
         this.frames = frames;
         this.currentFrame = frames.midflap;
+        this.width = this.currentFrame.width;
+        this.height = this.currentFrame.height;
         this.x = 20;
         this.y = (config.canvasHeight - this.currentFrame.height)/2;
         this.velocity = 0;
@@ -32,8 +36,28 @@ export class Bird implements Drawable {
         this.y += this.velocity * timeDelta;
     };
 
+    #lookUp = (context: CanvasRenderingContext2D) => {
+        const translX = this.x + this.width/2;
+        const translY = this.y + this.height/2;
+        context.translate(translX, translY);
+        context.rotate(-Math.PI/4);
+        context.translate(-translX, -translY);
+    }
+
+    #lookDown = (context: CanvasRenderingContext2D) => {
+        const translX = this.x + this.width/2;
+        const translY = this.y + this.height/2;
+        context.translate(translX, translY);
+        context.rotate(Math.PI/4);
+        context.translate(-translX, -translY);
+    }
+
     draw = (context: CanvasRenderingContext2D) => {
+        context.save();
+        if(this.velocity < 0) this.#lookUp(context);
+        else this.#lookDown(context);
         context.drawImage(this.currentFrame, this.x, this.y);
+        context.restore();
     };
 
 };
